51a6cadb6b744ae0d941e67b44b07dd171c29ca1
Jameson Aranda <jamesonaranda@gmail.com>
2016-03-08 15:19:50 -0700

    require!

---
+++ js/requireconfig.js
@@ -0,0 +1,12 @@
+require.config({
+	baseUrl: 'js/',
+	paths: {
+		'jquery': 'jquery-2.2.1.min.js',
+		'pouch': 'pouchdb-5.3.0.min.js'
+		'app': 'app.js'
+	}
+})
+
+require(['jquery', 'pouch', 'app'], function(){
+
+})
