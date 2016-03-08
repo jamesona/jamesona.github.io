6f3e4dad9774b4318f775a22300fc29cb16050ee
Jameson Aranda <jamesonaranda@gmail.com>
2016-03-08 15:44:06 -0700

    stuff...

--- js/app.js
+++ js/app.js
@@ -1,4 +1,14 @@
 (function(){
+	require.config({
+		baseUrl: 'js/',
+		paths: {
+			'jquery': 'jquery-2.2.1.min.js',
+			'pouch': 'pouchdb-5.3.0.min.js',
+			'app': 'app.js'
+		}
+	})
 
+	require(['jquery', 'pouch', 'app'], function($, db, app){
 
+	})
 })()
